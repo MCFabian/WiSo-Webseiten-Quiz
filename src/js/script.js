0a3a903eed45ced58284db48fe2ecaeeb2669c94
Fabian <60275181+MCFabian@users.noreply.github.com>
2020-09-08 10:36:29 +0200

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+//Funktion für Onload der Seite
+$(document).ready(function(){
+
+	//Multi CSS
+	$("#myid").css({
+		"background": "red",
+		"color" : "#fff"
+	});
+
+	// One CSS
+	$("#myid").css("font-size", "2rem");
+
+	//document.getElementById("myid").style.backgroundColor = "red";
+});
+
