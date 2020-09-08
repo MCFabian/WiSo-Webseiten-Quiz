6b40502ba7a149c850a098f3178d85f695121c8e
Fabian <60275181+MCFabian@users.noreply.github.com>
2020-09-08 14:46:36 +0200

    Quiz Einbindung
    Elemente für Quiz geschaffen

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+
+
+document.getElementById("next").addEventListener("click", function(){
+	var name = document.getElementById("name").value;
+
+	if(name == "") {
+		document.getElementById("welcome").innerHTML = "Lass uns loslegen!";
+	}
+
+	else {
+		document.getElementById("welcome").innerHTML = name +", lass uns loslegen!";
+	}
+});
+
+
+
