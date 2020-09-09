192779bb017e9873761bebff4aa8204e295df386
Fabian <60275181+MCFabian@users.noreply.github.com>
2020-09-09 14:21:48 +0200

    Added some Questions and Counter function

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,6 +1,4 @@
 // JavaScript Document
-
-
 document.getElementById("next").addEventListener("click", function(){
 	var name = document.getElementById("name").value;
 
@@ -11,7 +9,59 @@ document.getElementById("next").addEventListener("click", function(){
 	else {
 		document.getElementById("welcome").innerHTML = name +", lass uns loslegen!";
 	}
+
 });
+
+// INSG
+var score;
+//var labelfalse = classList.add("label-false");
+//var labeltrue = classList.add("label-true");
+
+
+// FRAG 1
+function getone() {
+	var one = document.getElementById("qu-one-ans-one");
+	var two = document.getElementById("qu-one-ans-two");
+	var three = document.getElementById("qu-one-ans-three");
+	var stepone;
+
+	if(three.checked){
+		stepone = 1;
+		alert("Richtig");
+	}
+
+	else {
+		stepone = 0;
+		alert("Falsch");
+	}
+
+	return stepone;
+}
+
+function gettwo(){
+	var one = document.getElementById("qu-two-ans-one");
+	var two = document.getElementById("qu-two-ans-two");
+
+	if(two.checked){
+		steptwo = 1;
+		alert("Richtig");
+	}
+
+	else {
+		steptwo = 0;
+		alert("Falsch");
+	}
+
+	return steptwo;
+}
+
+function getscore(){
+	score = getone() +gettwo();
+	alert(score);
+}
+
+
+
 
 
 
